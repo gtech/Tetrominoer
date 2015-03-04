f12bb02c5ca69ac26c4d635cad9fc69ab8617008
Will Duncan <will@aeondynamics.com>
2015-03-04 01:13:06 -0800

    Finished mapping a config to space array, and shifting it across the array for all its posibilities, haven't debugged it yet

--- lib/tetrominoer/block.rb
+++ lib/tetrominoer/block.rb
@@ -1,114 +1,120 @@
-class Block
-  @CONFIGURATIONS = Hash.new()
-  @ROWS = Int
-  @COLUMNS = Int
-  @PERMUTATIONS = Int
-  attr_reader :CONFIGURATIONS, :ROWS, :COLUMNS, :PERMUTATIONS
-end
+module Tetrominoer
 
-class O < Block
-  def initialize
-    @PERMUTATIONS = 1
-    @CONFIGURATIONS = 
-    {1=>
-     {
-      rows=>2,
-      columns=>2,
-      config=>[1,1,1,1]
-     }}
-
-# permutations=> 1
-#                        rows=> 2
-#                        columns=> 2
-                       
-
-#                        1=>[[1,1],
-#                            [1,1]]
-#                       }
-#     @CONFIGURATIONS = [1,1,1,1]
+  #Major class for creating blocks, every block has these features.
+  class Block
+    @CONFIGURATIONS = Hash.new()
+    @ROWS = Int
+    @COLUMNS = Int
+    @PERMUTATIONS = Int
+    attr_reader :CONFIGURATIONS, :ROWS, :COLUMNS, :PERMUTATIONS
   end
-end
 
-class L < Block
- def initialize
-    @PERMUTATIONS = 4
-    @CONFIGURATIONS = 
-      {1=>
-       {
-        rows=>3,
-        columns=>2,
-        config=>[0,1,0,1,1,1]
-       }
+
+  #2 by 2 block
+  class O < Block
+    def initialize
+      @PERMUTATIONS = 1
+      @CONFIGURATIONS = 
+        {1=>
+        {
+          rows=>2,
+          columns=>2,
+          config=>[1,1,1,1]
+        }}
+
+      # permutations=> 1
+      #                        rows=> 2
+      #                        columns=> 2
+      
+
+      #                        1=>[[1,1],
+      #                            [1,1]]
+      #                       }
+      #     @CONFIGURATIONS = [1,1,1,1]
+    end
+  end
+
+  class L < Block
+    def initialize
+      @PERMUTATIONS = 4
+      @CONFIGURATIONS = 
+        {1=>
+        {
+          rows=>3,
+          columns=>2,
+          config=>[0,1,0,1,1,1]
+        }
       }
-    
+      
 
-    # @CONFIGURATIONS = { 1=>[[0,1],
-    #                         [0,1],
-    #                         [1,1]],
+      # @CONFIGURATIONS = { 1=>[[0,1],
+      #                         [0,1],
+      #                         [1,1]],
 
-    #                    2=>[[1,0,0],
-    #                        [1,1,1]],
-                       
-    #                    3=>[[1,1],
-    #                        [1,0],
-    #                        [1,0]],
-                       
-    #                    4=>[[1,1,1],
-    #                        [0,0,1]],
-    #                   }
+      #                    2=>[[1,0,0],
+      #                        [1,1,1]],
+      
+      #                    3=>[[1,1],
+      #                        [1,0],
+      #                        [1,0]],
+      
+      #                    4=>[[1,1,1],
+      #                        [0,0,1]],
+      #                   }
+    end
   end
-end
 
-class R < Block
-  def initialize
-    @CONFIGURATIONS = { 1=>[[1,0],
-                            [1,0],
-                            [1,1]],
+  class R < Block
+    def initialize
+      @CONFIGURATIONS = { 1=>[[1,0],
+                              [1,0],
+                              [1,1]],
 
-                       2=>[[1,1,1],
-                           [1,0,0]],
-                       
-                       3=>[[1,1],
-                           [0,1],
-                           [0,1]],
-                       
-                       4=>[[0,0,1],
-                           [1,1,1]],
-                      }
+        2=>[[1,1,1],
+            [1,0,0]],
+        
+        3=>[[1,1],
+            [0,1],
+            [0,1]],
+        
+        4=>[[0,0,1],
+            [1,1,1]],
+      }
+    end
   end
-end
 
-class I < Block
-  def initialize
-    @CONFIGURATIONS = {1=> [[1,1,1,1]],
-                       2=>[[1],
-                           [1],
-                           [1],
-                           [1]],
-                      }
+  class I < Block
+    def initialize
+      @CONFIGURATIONS = {1=> [[1,1,1,1]],
+        2=>[[1],
+            [1],
+            [1],
+            [1]],
+      }
+    end
   end
-end
 
-class S < Block
-  def initialize
-    @CONFIGURATIONS = { 1=>[[0,1,1],
-                            [1,1,0]],
-                       
-                       2=>[[1,0],
-                           [1,1],
-                           [0,1]],
-                      }
+  class S < Block
+    def initialize
+      @CONFIGURATIONS = { 1=>[[0,1,1],
+                              [1,1,0]],
+        
+        2=>[[1,0],
+            [1,1],
+            [0,1]],
+      }
+    end
   end
-end
 
-class Z < Block
-  def initialize
-    @CONFIGURATIONS = { 1=>[[1,1,0],
-                            [0,1,1]],
-                       
-                       2=>[[0,1],
-                           [1,1],
-                           [1,0]],
-                      }
+  class Z < Block
+    def initialize
+      @CONFIGURATIONS = { 1=>[[1,1,0],
+                              [0,1,1]],
+        
+        2=>[[0,1],
+            [1,1],
+            [1,0]],
+      }
+    end
   end
 end
